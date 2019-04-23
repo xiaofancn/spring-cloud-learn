c9e0077bf0cc1d3900200509f5503672c0391405
fanzj <fanzj@fuwu.work>
2019-04-23 10:20:12 +0800

    websocket 示例，结合redis sub/pub

--- web/src/main/java/org/fansxnet/web/config/WebFluxConfig.java
+++ web/src/main/java/org/fansxnet/web/config/WebFluxConfig.java
@@ -1,8 +1,14 @@
 package org.fansxnet.web.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.fansxnet.web.controller.ChatController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.http.CacheControl;
 import org.springframework.web.reactive.HandlerMapping;
 import org.springframework.web.reactive.config.EnableWebFlux;
@@ -25,6 +31,7 @@ import java.util.concurrent.TimeUnit;
  * @CreateDate: Created in 2019/3/15 23:44 <br>
  * @Author: <a href="xiaofancn@qq.com">abc</a>
  */
+@Slf4j
 @EnableWebFlux
 @Configuration
 public class WebFluxConfig implements WebFluxConfigurer {
@@ -40,10 +47,13 @@ public class WebFluxConfig implements WebFluxConfigurer {
     }
 
 
+    @Autowired
+    ChatController chatController;
+
     @Bean
     public HandlerMapping handlerMapping() {
         Map<String, WebSocketHandler> map = new HashMap<>();
-        map.put("/chat", new ChatController());
+        map.put("/chat", chatController);
         SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
         mapping.setUrlMap(map);
         mapping.setOrder(-1); // before annotated controllers
@@ -58,5 +68,16 @@ public class WebFluxConfig implements WebFluxConfigurer {
     @Bean
     public WebSocketService webSocketService() {
         return new HandshakeWebSocketService(new ReactorNettyRequestUpgradeStrategy());
+    }
+
+    @Bean
+    ReactiveRedisTemplate<String, String> redisOperations(ReactiveRedisConnectionFactory factory) {
+        log.info(factory.toString());
+        RedisSerializationContext.SerializationPair<String> stringSerializationPair = RedisSerializationContext.SerializationPair
+                .fromSerializer(RedisSerializer.string());
+        RedisSerializationContext.RedisSerializationContextBuilder<String, String> builder =
+                RedisSerializationContext.newSerializationContext(stringSerializationPair);
+        RedisSerializationContext<String, String> context = builder.value(stringSerializationPair).build();
+        return new ReactiveRedisTemplate<>(factory, context);
     }
 }
--- web/src/main/java/org/fansxnet/web/controller/ChatController.java
+++ web/src/main/java/org/fansxnet/web/controller/ChatController.java
@@ -1,11 +1,17 @@
 package org.fansxnet.web.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.ReactiveRedisMessageListenerContainer;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.WebSocketSession;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import javax.annotation.PostConstruct;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -16,30 +22,55 @@ import java.util.concurrent.ConcurrentHashMap;
  * @Author: <a href="xiaofancn@qq.com">abc</a>
  */
 @Slf4j
+@Component
 public class ChatController implements WebSocketHandler {
+    final String topicName = "chat";
     //静态变量，用来记录当前在线连接数。应该把它设计成线程安全的。
-    private static Map<String, WebSocketSession> sessionMap = new ConcurrentHashMap<>();
+    public static Map<String, WebSocketSession> sessionMap = new ConcurrentHashMap<>();
 
     @Override
     public Mono<Void> handle(WebSocketSession session) {
-        sessionMap.put(session.getId(), session);
-        broadcast(session.getId() + "进来了。总人数为" + sessionMap.size());
-        log.info("记录连接{}", session.getId());
-        return session.receive().doOnComplete(() -> {
-                    sessionMap.remove(session.getId());
-                    broadcast(session.getId() + "离开了。总人数为" + sessionMap.size());
-                }
-        ).doOnNext(webSocketMessage -> {
-        }).concatMap(webSocketMessage -> {
-            broadcast(webSocketMessage.getPayloadAsText());
-            return Flux.empty();
-        }).then();
+        return session.receive()
+                .log()
+                .doOnSubscribe(webSocketMessage -> {
+                    log.info("记录连接{}", session.getId());
+                    sessionMap.put(session.getId(), session);
+                    reactiveRedisTemplate.convertAndSend(topicName, session.getId() + "进来了。总人数为" + sessionMap.size()).toProcessor();
+                })
+                .flatMap(webSocketMessage -> {
+                    String payTxt = session.getId()+":"+webSocketMessage.getPayloadAsText();
+                    log.info("{}", payTxt);
+                    return reactiveRedisTemplate.convertAndSend(topicName, payTxt).toProcessor();
+                })
+                .doOnComplete(() -> {
+                            sessionMap.remove(session.getId());
+                            String msg = session.getId() + "离开了。总人数为" + sessionMap.size();
+                            reactiveRedisTemplate.convertAndSend(topicName, msg).toProcessor();
+                            log.info(msg);
+                        }
+                )
+                .then();
+
     }
 
-    private void broadcast(String text) {
-        sessionMap.values().forEach(session -> {
-            log.info("广播内容{}", text);
-            session.send(Mono.just(session.textMessage(text))).toProcessor();
-        });
+    @Autowired
+    ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
+
+
+    @PostConstruct
+    public void init() {
+        ReactiveRedisMessageListenerContainer container = new ReactiveRedisMessageListenerContainer(reactiveRedisTemplate.getConnectionFactory());
+        container.receive(ChannelTopic.of(topicName))
+                .log()
+                .switchIfEmpty(Mono.error(new IllegalArgumentException()))
+                .subscribe(stringStringMessage -> {
+                    //一个线程里面的操作
+                    Flux.fromIterable(sessionMap.values())
+                            .map(webSocketSession ->
+                                    webSocketSession.send(Mono.just(webSocketSession.textMessage(stringStringMessage.getMessage()))).toProcessor()
+                            ).subscribe();
+                });
+
+
     }
 }
