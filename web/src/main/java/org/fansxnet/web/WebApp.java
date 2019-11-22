d4513606af6c9d78a7a0540b604c1732099e76cb
fanzj <fanzj@fuwu.work>
2019-11-22 15:43:13 +0800

    swagger flux mono

--- web/src/main/java/org/fansxnet/web/WebApp.java
+++ web/src/main/java/org/fansxnet/web/WebApp.java
@@ -1,10 +1,21 @@
 package org.fansxnet.web;
 
+import com.fasterxml.classmate.ResolvedType;
+import com.fasterxml.classmate.TypeResolver;
+import com.fasterxml.classmate.types.ResolvedArrayType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.method.HandlerMethod;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import springfox.documentation.spring.web.readers.operation.HandlerMethodResolver;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @Description: <br>
@@ -14,9 +25,43 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableFeignClients(basePackages = {"org.fansxnet.web.client"})
+@EnableSwagger2
 @EnableDiscoveryClient
 public class WebApp {
     public static void main(String[] args) {
         SpringApplication.run(WebApp.class, args);
+    }
+
+    /**
+     * 擦除Mono  Flux ResponseEntity 相应类型
+     * @param resolver
+     * @return
+     */
+    @Bean
+    @Primary
+    public HandlerMethodResolver fluxMethodResolver(TypeResolver resolver) {
+        return new HandlerMethodResolver(resolver) {
+            @Override
+            public ResolvedType methodReturnType(HandlerMethod handlerMethod) {
+                ResolvedType retType = super.methodReturnType(handlerMethod);
+
+                // we unwrap Mono, Flux, and as a bonus - ResponseEntity
+                while (
+                        retType.getErasedType() == Mono.class
+                                || retType.getErasedType() == Flux.class
+                                || retType.getErasedType() == ResponseEntity.class
+                ) {
+                    if ( retType.getErasedType() == Flux.class ) {
+                        // treat it as an array
+                        ResolvedType type = retType.getTypeBindings().getBoundType(0);
+                        retType = new ResolvedArrayType(type.getErasedType(), type.getTypeBindings(), type);
+                    } else {
+                        retType = retType.getTypeBindings().getBoundType(0);
+                    }
+                }
+
+                return retType;
+            }
+        };
     }
 }
